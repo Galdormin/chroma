a717f4c12adc2ed63a952b77ad7f96be2b3902e7
Galdormin <theoplenet@gmail.com>
2026-02-26 23:08:52 +0100

    add character spawn to ldtk

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,9 +1,11 @@
 //! Code for the player character
 
 use bevy::prelude::*;
+use bevy_ecs_ldtk::assets::LdtkProject;
 
 use crate::{
     PausableSystems,
+    asset_collection::LevelAssets,
     ldtk::GameColor,
     player::{
         movement::CharacterMovementBundle, physics::CharacterPhysicsBundle,
@@ -43,15 +45,37 @@ pub fn spawn_character(
     material_asets: ResMut<Assets<ColorMaterial>>,
     mesh_assets: ResMut<Assets<Mesh>>,
     maybe_player: Option<Single<Entity, With<Player>>>,
+    level_assets: Res<LevelAssets>,
+    ldtk_project_assets: Res<Assets<LdtkProject>>,
 ) {
-    let shape = Capsule2d::new(8.0, 10.0);
+    // Find the unique Spawn entity
+    let ldtk_project = ldtk_project_assets
+        .get(level_assets.world.id())
+        .expect("Project should be loaded by then.");
 
+    let spawn_pos = ldtk_project
+        .json_data()
+        .toc
+        .iter()
+        .filter(|entry| entry.identifier == "Spawn")
+        .map(|entry| {
+            entry
+                .instances_data
+                .first()
+                .map(|i| IVec2::new(i.world_x, -i.world_y).as_vec2())
+        })
+        .next()
+        .flatten()
+        .unwrap_or(Vec2::new(445., -200.));
+
+    // Create the character bundle
+    let shape = Capsule2d::new(8.0, 10.0);
     let bundle = (
         Player,
         CharacterVisualBundle::new(shape, GameColor::Grey, mesh_assets, material_asets),
         CharacterMovementBundle::new(10.0, 4.0, 0.3, 0.3),
         CharacterPhysicsBundle::new(shape),
-        Transform::from_xyz(1272.0, -200.0, 3.0),
+        Transform::from_translation(spawn_pos.extend(3.0)),
     );
 
     if let Some(player) = maybe_player {
